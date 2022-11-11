eb7ed6815ca1243f637788095cc88ad028efe2ef
kanghyew0n <104333249+kanghyew0n@users.noreply.github.com>
2022-11-11 22:29:12 +0900

    [section1 & 3] 파일 기반 라우팅

---
+++ udemy-nextjs/pages/index.js
@@ -0,0 +1,7 @@
+const HomePage = () => {
+  return (
+    <div><h1>The home page</h1></div>
+  )
+}
+
+export default HomePage
---
+++ udemy-nextjs/pages/portfolio/index.js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+const PortfolioPage = () => {
+  return (
+    <div>
+      <h1>PortfolioPage</h1>
+      <ul>
+        <li>
+          <Link href="/portfolio/list">portfolio list</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default PortfolioPage;
